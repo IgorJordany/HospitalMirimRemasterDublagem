5eb7fa303565ff96c093fb34973537ddd1b23a2b
Unknown <igorjordany@gmail.com>
2019-01-03 23:48:40 -0300

    Dublagem e botão OK

---
+++ Assets/Scripts/Dublagem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dublagem : MonoBehaviour {
+
+	public void MuteDublagem () {
+		//AudioListener.pause = true;
+		AudioListener.volume = 0;
+	}
+	public void DesmuteDublagem () {
+		//AudioListener.pause = true;
+		AudioListener.volume = 2;
+	}
+}
